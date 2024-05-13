69e9584a83d227e52a274c8eafc566718d04b1fa
mariakalendarova82 <mariakalendarova82@gmail.com>
2024-05-13 18:29:54 +0300

    Added salary calculation

--- src/main/java/project/cashier/Cashier.java
+++ src/main/java/project/cashier/Cashier.java
@@ -4,17 +4,17 @@
 
 package project.cashier;
 
-public class Cashier {
+public class Cashier extends Salary{
     private String name_of_cashier;
     private long id_of_cashier;
-    private double salary_of_cashier_per_hour;
-    private int hour_worked;
+    //private double salary_of_cashier_per_hour;
+    //private int hour_worked;
 
-    public Cashier(String name_of_cashier, long id_of_cashier, double salary_of_cashier_per_hour, int hour_worked) {
+    public Cashier(String name_of_cashier, long id_of_cashier) {
         this.name_of_cashier = name_of_cashier;
         this.id_of_cashier = id_of_cashier;
-        this.salary_of_cashier_per_hour = salary_of_cashier_per_hour;
-        this.hour_worked = hour_worked;
+        //this.salary_of_cashier_per_hour = salary_of_cashier_per_hour;
+        //this.hour_worked = hour_worked;
     }
 
     public String getName_of_cashier() {
@@ -25,34 +25,24 @@ public class Cashier {
         return id_of_cashier;
     }
 
-    public double getSalary_of_cashier_per_hour() {
-        return salary_of_cashier_per_hour;
-    }
-
-    public int getHour_worked() {
-        return hour_worked;
-    }
-
-    public double calculateMonthlySalary(){
-        double salary = 0.0;
-        if(getHour_worked() > 0){
-            salary = getHour_worked()*getSalary_of_cashier_per_hour();
-            return salary;
-        }
-        else {
-            System.out.println("Cashier has not worked any hours this month.");
-            return 0;
-        }
-    }
-
     @Override
     public String toString() {
         return "Cashier{" +
                 "name_of_cashier='" + name_of_cashier + '\'' +
                 ", id_of_cashier=" + id_of_cashier +
-                ", salary_of_cashier_per_hour=" + salary_of_cashier_per_hour +
-                ", hour_worked=" + hour_worked +
                 '}';
     }
+
+
+    //    public double calculateMonthlySalary(){
+//        double salary = 0.0;
+//        if(getHour_worked() > 0){
+//            salary = getHour_worked()*getSalary_of_cashier_per_hour();
+//            return salary;
+//        }
+//        else {
+//            System.out.println("Cashier has not worked any hours this month.");
+//            return 0;
+//        }
 }
 
---
+++ src/main/java/project/cashier/Salary.java
@@ -0,0 +1,16 @@
+package project.cashier;
+
+public class Salary implements SalaryCalculator{
+    @Override
+    public double calculatingSalary(double salary_per_hour, double hours_worked) {
+            double salary = 0.0;
+            if(hours_worked > 0){
+                salary = hours_worked * salary_per_hour;
+                return salary;
+            }
+            else {
+                System.out.println("Cashier has not worked any hours this month.");
+                return 0;
+            }
+    }
+}
